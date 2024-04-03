9b447521611c95d88e483e8074b2b3f849a03f5e
vanshchauhan1310 <112659160+vanshchauhan1310@users.noreply.github.com>
2024-04-03 22:23:10 +0530

    Rotated Array

---
+++ array/RotatedArray.java
@@ -0,0 +1,31 @@
+package array;
+
+public class RotatedArray {
+
+    public static void Reverse(int start,int last,int nums[]){
+        while(start < last) {
+            int temp = nums[start];
+            nums[start] = nums[last];
+            nums[last] = temp;
+            start++;
+            last--;
+
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,5,1};
+        int k = 3;
+        System.out.println("rotated array");
+
+        int start = 0;
+        int last = nums.length-1;
+
+        Reverse(start, last, nums);
+        Reverse(start,k-1,nums);
+        Reverse(k, last, nums);
+
+        for(int i=0;i<nums.length;i++){ 
+            System.out.println(nums[i]);
+        }
+    }
+}
