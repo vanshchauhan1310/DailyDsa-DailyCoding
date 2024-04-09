fd2d72b05f3f0cb1fc032d96862b63204dfb017a
vanshchauhan1310 <112659160+vanshchauhan1310@users.noreply.github.com>
2024-04-09 19:41:20 +0530

    Best time to buy and sell stock

---
+++ array/Besttimetobuyandsell.java
@@ -0,0 +1,25 @@
+package array;
+
+public class Besttimetobuyandsell {
+    
+
+    public static void main(String[] args) {
+    int arr[] = {7,1,3,2,4,6,5};
+    
+    int maxprofit = 0;
+    int minprice = 0;
+
+    for(int i=1;i<arr.length;i++){
+        int profit = arr[i]-arr[minprice];
+        if(profit>maxprofit){
+            maxprofit = profit;
+        }
+        if(arr[i]<arr[minprice]){
+            minprice = i;
+        }
+
+    }
+
+    System.out.println(maxprofit);
+    }
+}
