5cc16d076ebe32416455dfbbcceaed2cb6203fa2
vanshchauhan1310 <112659160+vanshchauhan1310@users.noreply.github.com>
2024-04-16 22:06:30 +0530

    Productofarray Added

---
+++ array/ProductOfArrayExceptSelf.java
@@ -0,0 +1,24 @@
+package array;
+
+public class ProductOfArrayExceptSelf {
+  public static void main(String[] args) {
+    int[] nums = {1,2,3,4};
+
+    int n = nums.length;
+    int[] result = new int[n];
+    int totalProduct = 1;
+
+    for (int num : nums) {
+        totalProduct *= num;
+    }
+    
+
+    for (int i = 0; i < n; i++) {
+        result[i] = totalProduct / nums[i];
+    }
+    
+    for(int i=0;i<n;i++){
+        System.out.println(result[i]);
+    }
+}
+}
