ff50d21bceca03f7c004c535871dd249a04e9c0c
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-05-19 14:17:32 +0530

    Started Learning Php

---
+++ DailyCoding/LearnPhp/Day1/Basic.php
@@ -0,0 +1,29 @@
+<?php
+echo 10+10;
+echo "<br>"; // this is used to break the line 
+echo "My name is Vansh Raj Chauhan <br>";
+echo "I am a student of B-Tech <br>";
+
+$clg = "upes"; // this is how we used to declare variable 
+
+echo "I am doing my graduation from $clg <br>";
+
+$address = "India"; 
+$Address = "Haryana";  
+$ADDRESS = "Gurgaon";
+
+const Data = "I am a constant Data";
+define("Data", "I am also a constant data");
+
+echo "I am From $address , $Address , $ADDRESS";
+
+echo "Their Are 8 type of data type in php <br>";
+echo "1. String <br>";
+echo "2. Integer <br>";
+echo "3. Float <br>";
+echo "4. Boolean <br>";
+echo "5. Array <br>";
+echo "6. Object <br>";
+echo "7. Null <br>";
+echo "8. Resource -- This is used for special functionalities like db connection <br>";
+?>
---
+++ DailyCoding/LearnPhp/Day1/HtmlWithPhp.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Vansh Raj Chauhan";
+$clg = "upes";
+$branch = "cse";
+$clg_color = "blue";
+$branch_color = "green";
+?>
+
+<h1 style="color:red">My Name is <?php echo $name?></h1>
+<h2 style="color:<?php echo $clg_color?>">My College is <?php echo $clg?></h2>
+<h3 style="color:<?php echo $branch_color?>">My Branch is <?php echo $branch?></h3>
