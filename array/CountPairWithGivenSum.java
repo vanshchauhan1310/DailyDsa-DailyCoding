b97fb46534a2fc0cbd16d0e1a5f9b39d4e202307
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-05-19 10:49:22 +0530

    FindMissingNumber and CountPair Question is added

---
+++ array/CountPairWithGivenSum.java
@@ -0,0 +1,18 @@
+package array;
+
+public class CountPairWithGivenSum {
+    public static void main(String[] args) {
+        int arr [] = {1,5,7,1};
+        int sum = 6;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]+arr[j] == sum){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+        
+    }
+}
