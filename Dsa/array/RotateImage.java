15fcb574c8d4e800dd1f91c7d43463280eef0b4d
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-07-31 14:30:00 +0530

    Added Maximum sum

---
+++ Dsa/array/RotateImage.java
@@ -0,0 +1,47 @@
+package array;
+// problem 48
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+ 
+
+// Example 1:
+
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+
+public class RotateImage {
+    
+    class Solution {
+        public void rotate(int[][] matrix) {
+              int n = matrix.length;
+            
+         for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+               int temp = matrix[i][j];
+               matrix[i][j]=matrix[j][i];
+               matrix[j][i]=temp; 
+            }
+         }
+    
+    
+    
+          for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n / 2; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][n - 1 - j];
+                    matrix[i][n - 1 - j] = temp;
+                }
+            }
+    
+    
+    
+    
+        }
+    }
+
+
+}
