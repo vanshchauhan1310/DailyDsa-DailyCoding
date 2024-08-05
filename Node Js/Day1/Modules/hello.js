8ec9ab25ceda862e23c561b944f0dffca03099f2
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-08-05 18:30:00 +0530

    Modules Added

---
+++ Node Js/Day1/Modules/hello.js
@@ -0,0 +1,8 @@
+// const math = require('./Math')
+const {add,sub} = require("./Math") // this is another way to inport function 
+
+// console.log("Addition is equal to",math.add(4,5))
+// console.log("Subtraction is equal to",math.sub(4,5))
+
+console.log(add(4,5));  
+console.log(sub(4,5));  // this is Another way to use exported functionn 
