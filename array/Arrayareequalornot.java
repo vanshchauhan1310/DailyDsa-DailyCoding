b0173539cf621ac43d5b05b5b78f18d96071216e
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-08-10 22:19:03 +0530

    Arrays are Equal or Not Solved

--- array/Arrayareequalornot.java
+++ array/Arrayareequalornot.java
@@ -6,15 +6,18 @@ public class Arrayareequalornot {
         int arr[] = {1,3,2,4,5,6};
         int arr1[] = {4,2,5,3,6,1};
 
-       Arrays.sort(arr);
-       Arrays.sort(arr1);
+        Arrays.sort(arr);
+        Arrays.sort(arr1);
 
-       for(int i=0;i<arr.length;i++){
-        if(arr[i]!=arr1[i]){
-            System.out.println(false);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=arr1[i]){
+                System.out.println("Arrays are not equal");
+                }
+                
+            }
+                
+        System.out.println("Arrays are Equal");
+
         }
-
-       }
-       System.out.println(true);
     }
-}
+
