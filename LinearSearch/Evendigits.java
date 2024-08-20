9fe76c6e6490f729496cdbd0b042ddce9f683e53
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-08-21 00:03:11 +0530

    Fixed Evendigit of element

--- LinearSearch/Evendigits.java
+++ LinearSearch/Evendigits.java
@@ -1,6 +1,13 @@
 package LinearSearch;
 
 public class Evendigits {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 52};
+        int ans = iseven(arr);
+        System.out.println(ans);
+    }
+
     static int iseven(int [] arr){
         int count =0;
         for (int num : arr) {
@@ -22,7 +29,7 @@ public class Evendigits {
 
     static int extractdigit(int num){
         int count = 0;
-        while (num<0) {
+        while (num>0) {
             count ++;
             num = num/10;
         }
