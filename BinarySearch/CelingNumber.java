d4060a4a23966cb49e74a4a26f4673782808f2a5
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-08-20 14:30:00 +0530

    updated a question

--- BinarySearch/CelingNumber.java
+++ BinarySearch/CelingNumber.java
@@ -11,19 +11,25 @@ public class CelingNumber {
     public static int celing(int arr[], int target) {
         int start = 0;
         int end = arr.length - 1;
-        int result = -1;
     
         while (start <= end) {
             int mid = start + (end - start) / 2;
     
-            if (arr[mid] >= target) {
-                result = arr[mid];
+            if (target<arr[mid]) {
                 end = mid - 1;  
-            } else {
-                start = mid + 1;
+            }else if (target>arr[mid]) {
+                start = mid+1;
+            } 
+            
+            else {
+               return mid;
             }
         }
     
-        return result;
+        return start;
     }
 }
+
+
+// In case of celing number just return the start 
+// In case of floor number just return the end
