f57d7c2ecb5a8148164d7b65254a315b2d122c9e
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-08-21 20:47:19 +0530

    Fixed Some Bugs

--- LinearSearch/Evendigits.java
+++ LinearSearch/Evendigits.java
@@ -1,40 +1,32 @@
 package LinearSearch;
 
 public class Evendigits {
-
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 52};
-        int ans = iseven(arr);
+        boolean ans = hasEvenDigits(arr);
         System.out.println(ans);
     }
 
-    static int iseven(int [] arr){
-        int count =0;
+    static boolean hasEvenDigits(int[] arr){
         for (int num : arr) {
-            if (findnum(num)) {
-                count ++;                
+            if (hasEvenDigits(num)) {
+                return true;
             }
-        }
-
-        return count;
-    }
-
-    static boolean findnum(int num){
-        int digits = extractdigit(num);
-        if (digits % 2 == 0) {
-            return true;            
         }
         return false;
     }
 
-    static int extractdigit(int num){
+    static boolean hasEvenDigits(int num){
+        int digits = extractDigits(num);
+        return digits % 2 == 0;
+    }
+
+    static int extractDigits(int num){
         int count = 0;
-        while (num>0) {
-            count ++;
-            num = num/10;
+        while (num > 0) {
+            count++;
+            num = num / 10;
         }
         return count;
     }
-
-
 }
