509d76145434389053f54a253b8cd41377ab855f
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-08-31 23:21:43 +0530

    learned About Url Handling

--- Node Js/Day2/server.js
+++ Node Js/Day2/server.js
@@ -1,15 +1,21 @@
 const http = require('http')
 const fs = require('fs')
+const url = require("url")
 const myserver = http.createServer((req,res)=>{
     const log  = `${Date.now()} : ${req.url} New Request Received` 
+    const myurl = url.parse(req.url,true)
+    console.log(myurl)
     fs.appendFile('server.log',log+'\n', (err)=>{
-        switch(req.url){
+        switch(myurl.pathname){
             case '/':
                 res.end("Welcome to My server")
                 break
             case '/about':
                 res.end("This is about page");
                 break
+            case '/search':
+                const username = myurl.query.name
+                res.end(`You are searching for ${username}`)
             default:
                 res.end("404 error not found ")
             }
