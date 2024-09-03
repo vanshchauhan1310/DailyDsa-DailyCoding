e0201a9426fdf511553c41141403822b7cd8b994
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-09-04 00:59:58 +0530

    Learned About versioning in Express js

---
+++ Node Js/Day3/versioning.js
@@ -0,0 +1,17 @@
+// "express": "^4.19.2"
+
+// In Express their are 3 part 
+// 1 part => 4
+// 2 part => 19
+// 3 part => 2
+
+// so third part stand for Minor changes like typos this are optional updates
+
+// 2 part => fixed a critical bug or added new feature or added more security updates 
+// 1 part => Major Update because in this their may be some syntax changes 
+// so if you are building new project then you can update the version but 
+// if you are working on existing project then you should not update the version of express
+// Because if you update on existing project then your code will break 
+
+// ~ => install all recommended minor fixes only change 3 bit  
+// ^ => do not change major bit only change 2 bit
