52893a5bcaecc62b9339c2286a9509d8daf8855c
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-11-20 23:39:12 +0530

    Revised this question

--- Dsa/array/MediumArrayChallenge/Move0toEnd.java
+++ Dsa/array/MediumArrayChallenge/Move0toEnd.java
@@ -3,37 +3,20 @@ public class Move0toEnd {
 
     public static void main(String[] args) {
         int arr[] = {1,2,4,0,3,6,0,0,8,7};
-        int temp[] = new int[arr.length];
-        int n = arr.length;
-        int x = 0; 
-
+        int nonzero = 0;
+    
+    for(int i = 0;i<arr.length;i++){
+        if(arr[i]!=0){
+            arr[nonzero]=arr[i];
+            nonzero++;
+        }
+    }
+    
+    for(int i = nonzero;i<arr.length;i++){
+        arr[i]=0;
+    }
         
-        for(int i = 0;i<n;i++){
-            if(arr[i]!=0){
-                x++;
-            }
-        }
-
-        int j = 0;
-        for(int i = 0;i<n;i++){
-            if(arr[i]!=0){
-                temp[j] = arr[i];
-                j++;
-            }
-        }
-
-
-        for(int i=0;i<x;i++){
-            arr[i]=temp[i];
-        }
-
-
-        for(int i =x;i<n;i++){
-            arr[i]=0;
-        }
-
-
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
     }
