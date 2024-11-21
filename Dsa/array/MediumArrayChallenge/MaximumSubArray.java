825ec0102076ef6bc96bdca3d23bbfe638115836
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-11-21 22:09:48 +0530

    Revised The Question

--- Dsa/array/MediumArrayChallenge/MaximumSubArray.java
+++ Dsa/array/MediumArrayChallenge/MaximumSubArray.java
@@ -20,16 +20,10 @@
 
     class Solution {
         public int maxSubArray(int[] nums) {
-            // Initialize variables
             int maxSum = nums[0];
             int currentSum = nums[0];
             
-            // Start from the second element
             for (int i = 1; i < nums.length; i++) {
-                // For each number, we have two choices:
-                // 1. Add it to the existing subarray (currentSum + nums[i])
-                // 2. Start a new subarray from this number (nums[i])
-                // We choose the maximum of these two
                 currentSum = Math.max(nums[i], currentSum + nums[i]);
                 
                 // Update the maximum sum if current sum is larger
