726a56ad973cfb29cad6eb983ffd68eac1eef09d
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2025-04-02 23:34:02 +0530

    Added Linked List

--- Dsa/Linkedlist/LL.java
+++ Dsa/Linkedlist/LL.java
@@ -10,6 +10,18 @@ public class LL {
         this.size=0;
     }
 
+
+    public void Displayall(){
+        Node temp = Head;
+        while (temp != null){
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+
+        System.out.println("");
+        
+    }
+
     public void insertFirst(int Data){
         Node node = new Node(Data);
         node.next = Head;
@@ -24,15 +36,22 @@ public class LL {
 
     }
 
-    public void Displayall(){
+    public void insert(int Data,int index){
+        if (index == 0){
+            insertFirst(Data);
+        }
+        if (index == size) {
+            insertLast(Data);            
+        }
         Node temp = Head;
-        while (temp != null){
-            System.out.print(temp.val + "->");
+        for (int i = 0; i < index - 1; i++) {
             temp = temp.next;
         }
 
-        System.out.println("");
-        
+        Node node = new Node(Data, temp.next);
+        temp.next = node;
+
+        size++;
     }
 
     public void insertLast(int Data){
@@ -46,9 +65,50 @@ public class LL {
         Tail = node;
         
         size++;
-
-     
     }
+
+    public void DeleteFirst(){
+        if (Head == null){
+            Tail = null;
+        }
+
+        Head = Head.next;
+        size--;
+
+    }
+
+    public void DeleteLast(){
+        Node temp = Head;
+        // System.out.println(size);
+        for(int i = 0;i<size-2;i++){
+            temp = temp.next;
+        }
+        Tail = temp;
+        Tail.next = null;
+
+      size--;
+
+    }
+
+    public void Delete(int index){
+        if (index == 0) {
+            DeleteFirst();       
+        }
+
+        if (index == size - 1) {
+            DeleteLast();            
+        }
+
+        Node Temp = Head;
+        for (int i = 0; i < index - 1; i++) {
+            Temp = Temp.next;
+        }
+
+        Temp.next = Temp.next.next;
+
+        size--;
+    }
+
     private class Node{
         int val;
         Node next;
--- Dsa/Linkedlist/simplyLinkedlist.java
+++ Dsa/Linkedlist/simplyLinkedlist.java
@@ -5,14 +5,21 @@ public class simplyLinkedlist {
     
     public static void main(String[] args) {
         LL list = new LL();
-        list.insertLast(60);
+        list.insertLast(70);
         list.insertFirst(50);
         list.insertFirst(40);
         list.insertFirst(30);
         list.insertFirst(20);
         list.insertFirst(10);
-
+        list.insert(60,5);
+        list.Displayall();
+        list.DeleteFirst();
+        list.Displayall();
+        list.DeleteLast();
+        list.Displayall();
+        list.Delete(2);
         list.Displayall();
     }
+
 
 }
