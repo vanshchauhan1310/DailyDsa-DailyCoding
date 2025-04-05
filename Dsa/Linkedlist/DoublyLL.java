1c71f64ada974f7e0800a73e561b1ecd061563b4
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2025-04-05 22:27:55 +0530

    DLL Added

---
+++ Dsa/Linkedlist/DoublyLL.java
@@ -0,0 +1,13 @@
+package Linkedlist;
+
+public class DoublyLL {
+    
+    public static void main(String[] args) {
+        DLL list = new DLL();
+        list.InsertFirst(20);
+        list.Displayall();
+        list.InsertFirst(10);
+        list.Displayall();
+        
+    }
+}
